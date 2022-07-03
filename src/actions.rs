130e5ccc3f26bc56e37ddec42e3d730f466ed66f
Mohammed Essehemy <mohammedessehemy@gmail.com>
2022-07-03 23:05:39 +0200

    chore: enhance actions error logic

--- src/actions.rs
+++ src/actions.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, Responder,Result, error::ErrorInternalServerError, HttpResponse};
+use actix_web::{get, post, web, Responder, HttpResponse};
 use super::db::{
     connect::DbPool,
     dal::{create_product, list_products},
@@ -6,19 +6,28 @@ use super::db::{
 };
 
 #[post("/products")]
-async fn product_create(product: web::Json<NewCompleteProduct>, pool: web::Data<DbPool>) -> Result<impl Responder> {
+async fn product_create(product: web::Json<NewCompleteProduct>, pool: web::Data<DbPool>) -> impl Responder {
     let connection = pool.get().unwrap();
-	match create_product(product.clone(), &connection) {
-		Ok(_) => Ok(HttpResponse::Ok()),
-		Err(error) => Err(ErrorInternalServerError(error))
-	}
+	let product = product.into_inner();
+	let _create_product_result = web::block(move || create_product(product, &connection).unwrap())
+	.await
+	.map_err(|e| {
+            log::error!("{}", e);
+            HttpResponse::InternalServerError().finish()
+        })
+	.unwrap();
+	HttpResponse::Created()
 }
 
 #[get("/products")]
-async fn product_list(pool: web::Data<DbPool>) -> Result<impl Responder> {
+async fn product_list(pool: web::Data<DbPool>) -> impl Responder {
     let connection = pool.get().unwrap();
-	match list_products(&connection) {
-		Ok(products) => Ok(HttpResponse::Ok().json(products)),
-		Err(error) => Err(ErrorInternalServerError(error))
-	}
+	let products = web::block(move || list_products(&connection).unwrap())
+	.await
+	.map_err(|e| {
+            log::error!("{}", e);
+            HttpResponse::InternalServerError().finish()
+        })
+	.unwrap();
+	HttpResponse::Ok().json(products)
 }
