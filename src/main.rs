7874f74f2f6257866ced3b88e43845271a8b312c
Mohammed Essehemy <mohammedessehemy@gmail.com>
2022-07-07 01:39:09 +0200

    feat: add session middleware

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use actix_web::{middleware::Logger, web, App, http, HttpServer};
+use actix_web::{middleware::Logger, web, App, http, HttpServer, cookie::Key};
 use actix_cors::Cors;
+use actix_session::{
+    SessionMiddleware,
+    storage::CookieSessionStore
+};
 use dotenv::dotenv;
 use std::env;
 use env_logger::Env;
@@ -18,14 +22,19 @@ async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
     log::info!("starting HTTP server at {address}:{port}");
     HttpServer::new(move || {
-        let cors = Cors::default()
+        let cors_mw = Cors::default()
             .allowed_origin(env::var("CORS_ALLOWED_ORIGINS").unwrap_or("https://my_domain.com/".to_owned()).as_ref())
             .allowed_methods(vec!["GET", "POST"])
             .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
             .allowed_header(http::header::CONTENT_TYPE)
             .max_age(3600);
+        let secret_key = Key::generate();
+        let session_mw = SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
+        .cookie_secure(false)
+        .build();
         App::new()
-            .wrap(cors)
+            .wrap(session_mw)
+            .wrap(cors_mw)
             .app_data(web::Data::new(conn.clone()))
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
